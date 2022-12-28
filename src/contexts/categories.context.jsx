105c7ccd4f60f33f8399d192674a8d8d30bd1191
Thanh Tuan <tuandtse243@gmail.com>
2022-12-28 16:59:43 +0700

    using reducer with useContext to replace useState

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 
 import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
@@ -6,7 +6,7 @@ import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 // import SHOP_DATA from '../shop-data.js'
 
 export const CategoriesContext = createContext({
-    categoriesMap: {},
+    categoriesMap: null,
     
 });
 
@@ -18,7 +18,6 @@ export const CategoriesProvider = ({children}) => {
     useEffect(() => {
         const getCategoriesMap = async () => {
             const categoryMap = await getCategoriesAndDocuments();
-            console.log(categoryMap);
             setcategoriesMap(categoryMap);
         }
         getCategoriesMap();
--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -4,7 +4,6 @@ import CategoryPreview from '../../components/category-preview/category-preview.
 
 const CategoriesPreview = () => {
     const { categoriesMap } = useContext(CategoriesContext);
-
     return (
         <div>
             {
