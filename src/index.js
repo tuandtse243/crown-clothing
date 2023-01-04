43eadccf09ab9bd59134f3d637c2b3e32c596cb9
Thanh Tuan <tuandtse243@gmail.com>
2023-01-04 08:04:17 +0700

    responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import reportWebVitals from './reportWebVitals';
